56a2fa5e564168b356a8d5821ed619c8393262b2
Rukesh <193492872+pac-source@users.noreply.github.com>
2026-01-05 09:21:59 +0530

    dmenu fix

--- rc.lua
+++ rc.lua
@@ -538,13 +538,13 @@ globalkeys = mytable.join(
     awful.key({ modkey }, "p", function() menubar.show() end,
               {description = "show the menubar", group = "launcher"}),
     --]]
-    --[[ dmenu
-    awful.key({ modkey }, "x", function ()
+    -- dmenu
+    awful.key({ modkey }, "r", function ()
             os.execute(string.format("dmenu_run -i -fn 'Monospace' -nb '%s' -nf '%s' -sb '%s' -sf '%s'",
             beautiful.bg_normal, beautiful.fg_normal, beautiful.bg_focus, beautiful.fg_focus))
         end,
         {description = "show dmenu", group = "launcher"}),
-    --]]
+   
     -- alternatively use rofi, a dmenu-like application with more features
     -- check https://github.com/DaveDavenport/rofi for more details
     --[[ rofi
@@ -554,10 +554,10 @@ globalkeys = mytable.join(
         end,
         {description = "show rofi", group = "launcher"}),
     --]]
-    -- Prompt
+    --[[ Prompt
     awful.key({ modkey }, "r", function () awful.util.spawn("dmenu_run") end,
               {description = "run prompt", group = "launcher"}),
-
+   --]]
     awful.key({ modkey }, "x",
               function ()
                   awful.prompt.run {
--- themes/luigimansion/theme.lua
+++ themes/luigimansion/theme.lua
@@ -243,7 +243,7 @@ function theme.at_screen_connect(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.systray(),
             first,
-            theme.mpd.widget,
+            --theme.mpd.widget,
             --theme.mail.widget,
             --theme.fs.widget,
             bat.widget,
